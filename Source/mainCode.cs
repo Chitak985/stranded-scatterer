7cabed03d985863c060e02084f7b4fda807982bb
Chitak 985 <156968477+Chitak985@users.noreply.github.com>
2025-10-26 08:08:40 -0400

    Update mainCode.cs

--- Source/mainCode.cs
+++ Source/mainCode.cs
@@ -9,32 +9,38 @@ namespace StrandedScatterer
 	[KSPAddon(KSPAddon.Startup.EveryScene, false)]
 	public class StrandedScatterer: MonoBehaviour
 	{
-    public Camera farCamera, scaledSpaceCamera, nearCamera;
-
-    void Init()
+	    public Camera farCamera, scaledSpaceCamera, nearCamera;
+	
+	    void Init()
 		{
 			SetupMainCameras ();
+	
+	    	scaledSpaceCamera.gameObject.AddComponent<ParticleSystem>();
+	      	farCamera.gameObject.AddComponent<ParticleSystem>();
+	      	nearCamera.gameObject.AddComponent<ParticleSystem>();
 
-      scaledSpaceCamera.gameObject.AddComponent<ParticleSystem>();
-      farCamera.gameObject.AddComponent<ParticleSystem>();
-      nearCamera.gameObject.AddComponent<ParticleSystem>();
-
-      var camera1 = scaledSpaceCamera.gameObject.GetComponent<ParticleSystem>().main;
-      var camera2 = farCamera.gameObject.GetComponent<ParticleSystem>().main;
-      var camera3 = nearCamera.gameObject.GetComponent<ParticleSystem>().main;
-
-      camera1.startLifetime = 10f;
-
-      scaledSpaceCamera.gameObject.GetComponent<ParticleSystem>().Play();
-      farCamera.gameObject.GetComponent<ParticleSystem>().Play();
-      nearCamera.gameObject.GetComponent<ParticleSystem>().Play();
-    }
-
-    void SetupCameras()
-    {
-      scaledSpaceCamera = Camera.allCameras.FirstOrDefault (_cam => _cam.name == "Camera ScaledSpace");
+			scaledSpaceCamera.gameObject.GetComponent<ParticleSystem>().Stop();
+	      	farCamera.gameObject.GetComponent<ParticleSystem>().Stop();
+	      	nearCamera.gameObject.GetComponent<ParticleSystem>().Stop();
+	
+	      	var camera1 = scaledSpaceCamera.gameObject.GetComponent<ParticleSystem>().main;
+	      	var camera2 = farCamera.gameObject.GetComponent<ParticleSystem>().main;
+	      	var camera3 = nearCamera.gameObject.GetComponent<ParticleSystem>().main;
+	
+	      	camera1.startLifetime = 10.0f;
+			camera2.startLifetime = 10.0f;
+			camera3.startLifetime = 10.0f;
+	
+	      	scaledSpaceCamera.gameObject.GetComponent<ParticleSystem>().Play();
+	      	farCamera.gameObject.GetComponent<ParticleSystem>().Play();
+	      	nearCamera.gameObject.GetComponent<ParticleSystem>().Play();
+	    }
+	
+	    void SetupCameras()
+	    {
+	      	scaledSpaceCamera = Camera.allCameras.FirstOrDefault (_cam => _cam.name == "Camera ScaledSpace");
 			farCamera = Camera.allCameras.FirstOrDefault (_cam => _cam.name == "Camera 01");
 			nearCamera = Camera.allCameras.FirstOrDefault (_cam => _cam.name == "Camera 00");
-    }
-  }
+	    }
+  	}
 }
